a3b60e2ab90e244352d2880bcd08e44872222eec
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2023-04-30 18:44:05 +0530

    Pdf view fix

--- src/PdfView.tsx
+++ src/PdfView.tsx
@@ -47,28 +47,36 @@ function PdfView({ books }: Props) {
       </tr>
       <tr
         style={{
+          width: "100%",
           border: "1px solid #939292",
           backgroundColor: "#d3d3d3",
           padding: "3px",
         }}
       >
-        <td width="auto">#</td>
-        <td width="40%">Book</td>
-        <td width="31%">Author</td>
+        <td width="45%">Book</td>
+        <td width="33%">Author</td>
         <td width="10%">Language</td>
-        <td width="7%">Price</td>
+        <td width="5%">Price</td>
         <td width="auto">Quantity</td>
-        <td width="8%">Total</td>
+        <td width="5%">Total</td>
       </tr>
       {books.map((book, idx) => (
-        <tr key={idx} style={{ padding: "3px" }}>
-          <td width="auto">{idx + 1}</td>
-          <td width="40%">{book.Name}</td>
-          <td width="31%">{book.Author}</td>
+        <tr
+          key={idx}
+          style={{
+            width: "100%",
+            padding: "3px",
+          }}
+        >
+          <td width="45%">
+            {idx + 1}.&nbsp;&nbsp;
+            {book.Name}
+          </td>
+          <td width="33%">{book.Author}</td>
           <td width="10%">{book.Language}</td>
-          <td width="7%">{book.Price}</td>
+          <td width="5%">{book.Price}</td>
           <td width="auto">{book.Quantity}</td>
-          <td width="8%">{book.TotalPrice}</td>
+          <td width="5%">{book.TotalPrice}</td>
         </tr>
       ))}
       <hr />
@@ -81,7 +89,23 @@ function PdfView({ books }: Props) {
             paddingRight: 70,
           }}
         >
-          Total:{books.reduce((a, b) => a + b.TotalPrice, 0).toFixed(2)}
+          <strong>
+            Total:{books.reduce((a, b) => a + b.TotalPrice, 0).toFixed(2)}
+          </strong>
+        </td>
+      </tr>
+      <hr />
+      <tr style={{ width: "100%" }}>
+        <td
+          style={{
+            width: "8%",
+            fontSize: "10px",
+            textAlign: "right",
+            paddingTop: 70,
+            paddingRight: 70,
+          }}
+        >
+          <em>Signature</em>
         </td>
       </tr>
       <hr />
